f2ba07b7b0fc26e10de518d735087c35dc301584
Manuel Meyer <mmeyer@pivotal.io>
2016-10-30 10:02:17 +0000

    display version number at startup

--- guido-agent-core/src/main/java/org/guido/agent/AgentMain.java
+++ guido-agent-core/src/main/java/org/guido/agent/AgentMain.java
@@ -7,7 +7,7 @@ import org.guido.agent.transformer.logger.GuidoLogger;
 
 public class AgentMain {
 	  public static void premain(String args, Instrumentation instrumentation) {
-		  GuidoLogger.getLogger("main").debug("Guido agent loaded ...");
+		  GuidoLogger.getLogger("main").debug("Guido {} agent loaded ...", AgentMain.class.getPackage().getImplementationVersion());
 		  GuidoTransformer transformer = new GuidoTransformer();
 		  instrumentation.addTransformer(transformer);
 	  }
